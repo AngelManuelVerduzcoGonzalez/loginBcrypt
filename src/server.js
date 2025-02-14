f4c7148fcd7f11b42714822e112c0f195c07bfd1
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-02-13 19:45:18 -0600

    Users status endpoint

--- src/server.js
+++ src/server.js
@@ -72,6 +72,40 @@ app.get('/users/:username', (req, res) => {
             res.status(404).json({ error: 'User not found' });
         }
     });
+});
+
+app.post('/users/:username', (req, res) => { 
+    const username = req.params.username;
+    const filePath = path.join(__dirname, '../usuarios.json');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        let users = [];
+        if (data) {
+            users = JSON.parse(data);
+        }
+
+        const userIndex = users.findIndex(user => user.username === username);
+        if (userIndex === -1) {
+            return res.status(404).json({ error: 'User not found' });
+        } else {
+            users[userIndex].isActive = !users[userIndex].isActive;
+        }
+
+        fs.writeFile(filePath, JSON.stringify(users, null, 2), (err) => {
+            if (err) {
+                console.error('Error writing file:', err);
+                return res.status(500).json({ error: 'Internal Server Error' });
+            } else {
+                console.log('User saved successfully');
+                res.status(200).json({ message: 'User saved successfully' });
+            }
+        });
+    });
 })
 
 app.listen(port, () => {
